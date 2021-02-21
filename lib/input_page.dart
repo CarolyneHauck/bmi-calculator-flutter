aa6a611ac428257e6c056a5aee0083991c164dc9
CarolyneHauck <carolyne.carreira@gmail.com>
2021-02-20 22:47:47 -0300

    Add new widgtes

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,11 @@
+import 'package:bmi_calculator/icon_content.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const activeCardColour = Color(0xFF1D1E33);
-const bottomContainerHeight = 80.0;
+import 'constants.dart';
+
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,6 +13,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+  int height = 180;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,26 +23,77 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ReusableBox(
-                    colour: activeCardColour,
-                  ),
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableBox(
+                  onPress: () {
+                    setState(() {
+                      selectedGender = Gender.male;
+                    });
+                  },
+                  colour: selectedGender == Gender.male
+                      ? activeCardColour
+                      : inactiveCardColour,
+                  cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars, textGenre: 'HOMEM'),
                 ),
-                Expanded(
-                  child: ReusableBox(
-                    colour: activeCardColour,
-                  ),
+              ),
+              Expanded(
+                child: ReusableBox(
+                  onPress: () {
+                    setState(() {
+                      selectedGender = Gender.female;
+                    });
+                  },
+                  colour: selectedGender == Gender.female
+                      ? activeCardColour
+                      : inactiveCardColour,
+                  cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus, textGenre: 'MULHER'),
                 ),
-              ],
-            )),
-          Expanded(
-              child: ReusableBox(colour: activeCardColour,
+              ),
+            ],
           )),
           Expanded(
+            child: ReusableBox(
+              colour: activeCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'ALTURA',
+                    style: labelTextStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: <Widget>[
+                      Text(height.toString(), style: numberTextStyle),
+                      Text('cm', style: labelTextStyle),
+                    ],
+                  ),
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120.0,
+                    max: 220.0,
+                    activeColor: Color(0xFFEB1555),
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newValue) {
+                      setState(() {
+                        height = newValue.round();
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
@@ -49,30 +107,16 @@ class _InputPageState extends State<InputPage> {
                   ),
                 ),
               ],
-            )),
+            ),
+          ),
           Container(
-            color: Color(0xFFEB1555),
+            color: bottomContainerColour,
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: bottomContainerHeight,
           ),
         ],
       ),
-    );
-  }
-}
-
-class ReusableBox extends StatelessWidget {
-  ReusableBox({@required this.colour});
-
-  final Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: colour, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
